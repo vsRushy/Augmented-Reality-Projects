ce79ae739ac7ff3f0d7ab0c382a9ff464c2cae86
thedoctormarc <mdpdoctor7@hotmail.com>
2020-03-28 12:57:56 +0100

    Update project_01.py

--- Project1/project_01.py
+++ Project1/project_01.py
@@ -5,7 +5,7 @@ from math import hypot
 # Functions -------------------------------------------------------------------------
 
 # Source is what we want to find and tarjet is the object where we want to find the source
-def templateMatch(target, source):
+def templateMatch(target, source, scaleFactor = 1.0):
     target_height, target_width, _ = target.shape
     source_height, source_width, _ = source.shape
 
@@ -28,7 +28,12 @@ def templateMatch(target, source):
                 print("Match found!")
                 indexes.append([i, j])
             ssd = 0
-
+     
+    if(scaleFactor != 1.0): 
+        for i in range(len(indexes)): 
+            index_i = indexes[i]
+            index_i /= scaleFactor
+    
     return indexes
 
 # -----------------------------------------------------------------------------------
@@ -40,18 +45,20 @@ def CompareSSD(ssd, magnitude):
         return False
 
 def fillMatch(img, index_i, index_j, width, height): 
-    return cv2.rectangle(img, (index_j, index_i), (index_j + height, index_i + width), (0, 255, 0), thickness=1)  
+    return cv2.rectangle(img, (index_j, index_i), (index_j + height, index_i + width), (0, 255, 0), thickness=2)  
 
-target_image = cv2.imread("images/img2.png")
+scaleFactorUsed = 1
+target_image = cv2.imread("images/img1.png")
 target_image = target_image / 255
+target_image_resized = cv2.resize(target_image, (0,0), fx=scaleFactorUsed, fy=scaleFactorUsed) 
 
-
-source_image = cv2.imread("images/t1-img2.png")
+source_image = cv2.imread("images/t1-img1.png")
 source_image = source_image / 255
+source_image_resized = cv2.resize(source_image, (0,0), fx=scaleFactorUsed, fy=scaleFactorUsed) 
 cv2.imshow("Source Image", source_image)
 
 threshold = 0.1
-indexes_result = templateMatch(source_image, target_image)
+indexes_result = templateMatch(source_image_resized, target_image_resized, scaleFactorUsed)
 
 source_height_result, source_width_result, _ = source_image.shape
 
@@ -63,33 +70,27 @@ for i_i in range(len(indexes_result)):
 cv2.imshow("Target Image", target_image)
 
 
-# foundText = "Image Not Found"
-# if(operationSsd <= threshold): 
-#     foundText = "Image Found"
+foundText = "Image Not Found"
+if(len(rects) >= 1): 
+    foundText = "Image Found"
 
-# foundText += ", ssd: "
-# foundText += str(operationSsd)
-# foundText += " and threshold: "
-# foundText += str(threshold)
 
-# cv2.imshow("Result Image", operationResultImage)
-
-# text_image = np.zeros((50, 1050, 3), np.uint8)
-# font = cv2.FONT_HERSHEY_SIMPLEX
-# bottom_left_corner_text = (10, 35)
-# font_scale = 1
-# font_color = (255, 255, 255)
-# line_type = 2
-# cv2.putText(
-#     text_image,
-#     foundText,
-#     bottom_left_corner_text,
-#     font,
-#     font_scale,
-#     font_color,
-#     line_type
-#     )
-# cv2.imshow("Result", text_image)
+text_image = np.zeros((50, 1050, 3), np.uint8)
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottom_left_corner_text = (10, 35)
+font_scale = 1
+font_color = (255, 255, 255)
+line_type = 2
+cv2.putText(
+    text_image,
+    foundText,
+    bottom_left_corner_text,
+    font,
+    font_scale,
+    font_color,
+    line_type
+    )
+cv2.imshow("Result", text_image)
 
 # Input ---------
 key = cv2.waitKey(0)
