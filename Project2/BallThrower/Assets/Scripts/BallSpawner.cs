da929fc97a9c6a6c712016d360facac97f1fc1ea
vsRushy <gerardmarcosfreixas@gmail.com>
2020-05-09 13:34:09 +0200

    Fixed target size

---
+++ Project2/BallThrower/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float max_time = 7.0f;
+    private float current_time = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        current_time += Time.deltaTime;
+        if(current_time >= max_time)
+        {
+            Instantiate(prefab);
+            current_time = 0.0f;
+        }
+    }
+}
