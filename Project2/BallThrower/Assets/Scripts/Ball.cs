06806ee8843e0379065b02a8457b3622b5bb1583
thedoctormarc <mdpdoctor7@hotmail.com>
2020-05-10 15:16:35 +0200

    angles ok but force isn't

--- Project2/BallThrower/Assets/Scripts/Ball.cs
+++ Project2/BallThrower/Assets/Scripts/Ball.cs
@@ -7,7 +7,7 @@ public class Ball : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject arrow;
-    public float forceStrength = 0.05f; 
+    public float forceStrength = 5.0f; 
     private Rigidbody body;
     bool clicked = false;
 
@@ -22,36 +22,43 @@ public class Ball : MonoBehaviour
         SetArrow(); 
 
         // Activate rigidbody and impulse it with the arrow direction 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (clicked && Input.GetKeyUp(KeyCode.Mouse0))
         {
             body.useGravity = true;
-            float Z = arrow.transform.rotation.eulerAngles.z;
+            float Z = arrow.transform.localRotation.eulerAngles.z;
 
-            float X = Mathf.Cos(Z);
-            float Y = Mathf.Sin(Z); 
+            if (Z < 0)
+                Z = 360 + Z;
 
-            if ((Z > 90) && (Z <= 180))
+
+            float X = 0.0f, Y = 0.0f;
+            switch (Z)
             {
-                X = -Mathf.Cos(Z);
-                Y = Mathf.Sin(Z);
+                case float n when (Z <= 90):
+                    X = Mathf.Cos(Z);
+                    Y = Mathf.Sin(Z);
+                    break; 
+                case float n when ((Z > 90) && (Z <= 180)):
+                    X = -Mathf.Cos(Z);
+                    Y = Mathf.Sin(Z);
+                    break;
+                case float n when ((Z > 180) && (Z <= 270)):
+                    X = -Mathf.Cos(Z);
+                    Y = -Mathf.Sin(Z);
+                    break; 
+                case float n when (Z > 270):
+                    X = Mathf.Cos(Z);
+                    Y = -Mathf.Sin(Z);
+                    break;
             }
-            else if ((Z > 180) && (Z <= 270))
-            {
-                X = -Mathf.Cos(Z);
-                Y = -Mathf.Sin(Z);
-            }
-            else if(Z > 270)
-            {
-                X = Mathf.Cos(Z);
-                Y = -Mathf.Sin(Z);
-            }
-          
+
+
             Vector3 forceVector = new Vector3(X, Y, 0) * forceStrength; 
             body.AddForce(forceVector, ForceMode.Impulse); 
             arrow.transform.rotation = Quaternion.identity; 
             arrow.SetActive(false);
 
-            Debug.Log("Ball launched with force: " + forceVector); 
+            Debug.Log("Ball launched with Z rotation: " + Z + "and force: " + forceVector); 
         }
           
     }
@@ -70,15 +77,9 @@ public class Ball : MonoBehaviour
         else
         {
             // Mouse position
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 0;
-
-            // Angle between mouse and arrow
-            float angle = Mathf.Atan2(mousePos.y - arrow.transform.position.y, mousePos.x - arrow.transform.position.x);
-            angle *= (180 / Mathf.PI); 
-
-            // Rotate arrow
-            arrow.transform.rotation = Quaternion.Euler(0, 0, angle);
+            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(arrow.transform.position);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
 
         }
     }
