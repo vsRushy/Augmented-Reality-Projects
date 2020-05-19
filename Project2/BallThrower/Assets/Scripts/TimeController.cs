ece41b2d492a70e1a1df4f176c35405ac3148bf6
GuillemSanchez <36293100+GuillemSanchez@users.noreply.github.com>
2020-05-19 21:06:22 +0200

    Finale done

--- Project2/BallThrower/Assets/Scripts/TimeController.cs
+++ Project2/BallThrower/Assets/Scripts/TimeController.cs
@@ -7,11 +7,15 @@ public class TimeController : MonoBehaviour
 {
     Text text;
     float time = 60.0f;
+    GameObject finale;
+  
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
+        finale = GameObject.Find("Final_Canvas");
+        finale.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,7 +27,7 @@ public class TimeController : MonoBehaviour
 
         if(time <= 0.0f)
         {
-
+            finale.SetActive(true);
         }
     }
 }
