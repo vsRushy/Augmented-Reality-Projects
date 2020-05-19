1d456458361a8c4cf07899f1be25168c64485f80
vsRushy <gerardmarcosfreixas@gmail.com>
2020-05-19 09:41:51 +0200

    Display final score end screen

---
+++ Project2/BallThrower/Assets/FinalScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour
+{
+    Text final_score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        final_score.text = ScoreManager.score.ToString();
+    }
+}
