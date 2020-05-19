1b2a56ef21b0b91b32e7760cbf082942dfed51ec
vsRushy <gerardmarcosfreixas@gmail.com>
2020-05-19 09:45:13 +0200

    Fixed some errors

--- Project2/BallThrower/Assets/Scripts/FinalScore.cs
+++ Project2/BallThrower/Assets/Scripts/FinalScore.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalScore : MonoBehaviour
 {
+    Text final_score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        final_score.text = ScoreManager.score.ToString();
     }
 }
+
